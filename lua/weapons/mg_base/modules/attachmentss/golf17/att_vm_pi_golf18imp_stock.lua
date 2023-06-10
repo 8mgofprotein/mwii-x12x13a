e5530557079b67559b6fd4a8992a2dd545c0ec28
ZeR0-N <tenppppp@hotmail.com>
2023-06-11 00:56:08 +0700

    conversion

--- lua/weapons/mg_base/modules/attachmentss/golf17/att_vm_pi_golf18imp_stock.lua
+++ lua/weapons/mg_base/modules/attachmentss/golf17/att_vm_pi_golf18imp_stock.lua
@@ -1,7 +1,21 @@
-ATTACHMENT.Base = "att_barrel"
-ATTACHMENT.Name = "Impact Point Barrel"
+ATTACHMENT.Base = "att_conversion"
+ATTACHMENT.Name = "Impact Point"
 ATTACHMENT.Model = Model("models/kyo/att_vm_p24_pi_golf17_genshinimpactbar.mdl")
 ATTACHMENT.Icon = Material("viper/mw/attachments/icons/golf21/icon_attachment_pi_golf21_stock.vmt")
+ATTACHMENT.ExcludedAttachments = {"att_vm_pi_golf18skel_stock"}
+ATTACHMENT.ExcludedCategories = {"Barrels"}
+ATTACHMENT.Conversion = {
+	[6] = {"att_sight", "att_vm_2x_west02_holo", "att_vm_2x_west02", "att_vm_reflex_02", "att_vm_minireddot01_tall", "att_vm_minireddot02_tall", "att_vm_minireddot03_tall",
+    "att_vm_holo_west01", "att_vm_holo_west02", "att_vm_holo_east01", "att_vm_reflex_east01",
+    "att_vm_reflex_east02", "att_vm_reflex_west02_tall", "att_vm_reflex_west03",
+    "att_vm_thermal_east01", "att_vm_thermal_west01", "att_vm_thermal_east01_hybrid",
+    "att_vm_hybrid_west01", "att_vm_hybrid_west02", "att_vm_hybrid_west02_thermal", "att_vm_reflex_west04", "att_vm_2x_west01",
+    "att_vm_hybrid_west03", "att_vm_4x_east01",
+    "att_vm_4x_west01", "att_vm_4x_west02",
+    "att_vm_scope_mike14", "att_vm_scope_vz"},
+	[7] = {"att_barrel"},
+	[9] = {"att_laser", "att_vm_laser01", "att_vm_laser02", "att_vm_laser03"},
+}
 local BaseClass = GetAttachmentBaseClass(ATTACHMENT.Base)
 function ATTACHMENT:Stats(weapon)
     BaseClass.Stats(self, weapon)
@@ -14,6 +28,7 @@ function ATTACHMENT:Stats(weapon)
     weapon.Animations.Draw.Fps = weapon.Animations.Draw.Fps * 0.85
     weapon.Animations.Holster.Fps = weapon.Animations.Holster.Fps * 0.85
     weapon.Recoil.AdsMultiplier = weapon.Recoil.AdsMultiplier * 0.5
+	weapon.ViewModelOffsets.Aim.Pos = weapon.ViewModelOffsets.Aim.Pos + Vector(1.66, 0, -0.15)
 end
 
 function ATTACHMENT:PostProcess(weapon)
